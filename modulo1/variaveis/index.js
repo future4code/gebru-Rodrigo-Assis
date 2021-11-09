eb7a755cba17d79633b83f4e7a95196b1a7f0762
Digoleira <rodrigord100@gmail.com>
2021-11-09 20:51:57 -0300

    Atividade da aula de variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,82 @@
+//Exercícios de interpretação de código
+
+//1.
+
+//5, 10
+
+//1.
+
+//10, 10, 10
+
+//3.
+
+//horaDia, diariaReal
+
+//Exercícios de escrita de código
+
+//1.
+
+//let nome
+//let idade
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+// Deu undefined pq não atribui nenhum valor a variavel
+
+//e)
+
+//let nome = prompt("Qual seu nome ?")
+//let idade = prompt("Qual sua idade ?")
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+//Ele imprimiu os dois valores como string
+
+
+//g)
+
+//let nome = prompt("Qual seu nome ?")
+//let idade = prompt("Qual sua idade ?")
+
+//console.log ("Olá", nome, "você tem", idade, "anos")
+
+//2.
+
+//let oculos = prompt("você usa oculos ?")
+//let altura = prompt("você tem mais de 1,70 ?")
+//let estuda = prompt("você estuda ?")
+
+//let resp1 = oculos
+//let resp2 = altura
+//let resp3 = estuda
+
+//console.log ("você usa oculos ? - ",  resp1,)
+//console.log ("você tem mais de 1,70 ? - ", resp2,)
+//console.log ("você estuda ? - ", resp3,)
+
+//3.
+
+//let a = 10
+//let b = 25
+
+//let x = a
+
+//a = b
+//b = x
+
+//console.log("O novo valor de a é", a) 
+//console.log("O novo valor de b é", b) 
+
+
+//DESAFIO
+
+//let n1 = prompt("numero 1 ")
+//let n2 = prompt("numero 2")
+
+//let soma = Number(n1) + Number(n2)
+//let mult = Number(n1) * Number(n2)
+
+//console.log ("O primeiro número somado ao segundo número resulta em:", soma,".")
+//console.log ("O primeiro número multiplicado pelo segundo número resulta em:", mult,".")
