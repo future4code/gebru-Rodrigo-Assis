01358684db6e2e7e78f15ff63108e2c9c13ec590
Digoleira <rodrigord100@gmail.com>
2021-11-17 21:35:08 -0300

    Exercicio aula strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,65 @@
+/* Exercícios de interpretação de código
+
+1.
+
+a. undefined
+b. null
+c. 11
+d. 3
+e. [ 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
+f. 9
+
+2.
+
+SUB NUM ÔNBUS EM MRROCOS
+
+
+Exercícios de escrita de código
+
+1.
+
+const email = prompt('Digite seu email')
+const nome = prompt('Digite seu Nome')
+
+console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
+
+2.
+
+const comidas = ["pizza", "frango grelhado", "feijoada", "moqueca", "crepioca"]
+
+console.log(comidas)
+console.log("Essas são minhas comidas preferidas:")
+console.log(comidas[0])
+console.log(comidas[1])
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+
+DESAFIO:
+
+const desafio = prompt('Digite uma comida')
+
+const comidas = ["pizza", "frango grelhado", "feijoada", "moqueca", "crepioca"]
+
+comidas.splice(1, 1, desafio)
+
+console.log(comidas)
+
+3.
+const tar1 = prompt('Digite uma tarefa')
+const tar2 = prompt('Digite outra tarefa')
+const tar3 = prompt('Digite mais uma tarefa')
+
+const listaDeTarefas = [tar1, tar2, tar3]
+
+console.log(listaDeTarefas)
+
+const i = prompt('Digite um indice')
+
+listaDeTarefas.splice (0,i)
+
+console.log(listaDeTarefas) */
+
+
+
+
