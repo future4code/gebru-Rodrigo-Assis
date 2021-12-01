c9a1c5f26c65d8946d180583507a5f4bb95618ee
Digoleira <rodrigord100@gmail.com>
2021-11-30 22:31:57 -0300

    exercicio da aula de lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,17 @@
+/* Exercícios de interpretação de código
+
+1.
+
+O codigo adiciona 1 na variavel valor ate que esse valor seja menor que 5, e imprime o valor 4.
+
+2.
+
+a) Todos os numeros que são maiores que 18 
+b) Sim, guardar os valor em uma variavel 
+
+3. 
+
+Adciona um asterisco a cada linha inserida 
+
+Exercícios de escrita de código */
+
