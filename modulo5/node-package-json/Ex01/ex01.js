f88283758bd2a213f2e2dc6f2628a0acaa5e3122
Digoleira <rodrigord100@gmail.com>
2022-05-19 22:15:41 -0300

    Resolução Aula - Node e package.json

---
+++ modulo5/node-package-json/Ex01/ex01.js
@@ -0,0 +1,21 @@
+
+/* Tarefa de Casa */
+
+/* EX 01 */
+
+/* A */
+
+/* Usamos a propriedade process.argv */
+
+/* B */
+
+/* const nome = process.argv[2]
+const idade = Number(process.argv[3])
+console.log (`Olá, ${nome}! você tem ${idade} anos.` ) */
+
+/* C */
+
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+console.log (`Olá, ${nome}! você tem ${idade} anos.` )
+
