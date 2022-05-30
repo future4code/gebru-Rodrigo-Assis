e7ec5b788af2ff55f7bd43ba01d88f6880030366
Digoleira <rodrigord100@gmail.com>
2022-05-30 10:25:54 -0300

    Ex 01

---
+++ modulo5/lista-typescript/src/index.ts
@@ -0,0 +1,13 @@
+/* Ex 01
+
+function saudacao(nome:string, data:string){
+    
+    let dataparastring:string[] = data.split("/")
+    
+    return (`Olá me chamo ${nome}, nasci no dia ${dataparastring[0]} do mês de ${dataparastring[1]} do ano de ${dataparastring[2]}`)
+    
+}
+ 
+console.log(saudacao("Rodrigo","25/03/1996" )) */
+
+
