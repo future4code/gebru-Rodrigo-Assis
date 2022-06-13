82a87c64331b773715be2a5bad0d3ad67bf3da2e
Digoleira <rodrigord100@gmail.com>
2022-06-13 18:33:19 -0300

    Projeto Sistema Bancario

---
+++ modulo5/projeto-sistema-bancario/src/index.ts
@@ -0,0 +1,96 @@
+// dentro do index.ts, fazer importações e ativar o Express e Cors.
+
+import express, {Express, Request, Response} from 'express'
+import cors from 'cors'
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+// a função express() inicia a aplicação web com express
+// os .use() ativam os módulos de Bodyparser e Cors
+
+// esse código + essa importação para criar o servidor:
+// por performance, é bom o servidor ser o último trecho de código do documento
+
+
+import { AddressInfo } from "net";
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
+
+//--------------------------------------------------------------------------//
+
+
+app.post("/create", (req: Request, Res: Response) => {
+
+try {
+
+}
+catch{
+
+}
+
+})
+
+app.post("/create", (req: Request, Res: Response) => {
+
+    try {
+    
+    }
+    catch{
+    
+    }
+    
+        })
+
+app.post("/addBalance", (req: Request, Res: Response) => {
+
+        try {
+        
+        }
+        catch{
+        
+        }
+        
+        })
+
+app.get("/balance", (req: Request, Res: Response) => {
+
+            try {
+            
+            }
+            catch{
+            
+            }
+            
+        })
+
+app.get("/payment", (req: Request, Res: Response) => {
+
+            try {
+            
+            }
+            catch{
+            
+            }
+            
+        })
+
+app.get("/transfer", (req: Request, Res: Response) => {
+
+            try {
+            
+            }
+            catch{
+            
+            }
+            
+        })
